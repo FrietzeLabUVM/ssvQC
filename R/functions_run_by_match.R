44aa5179b3761c80569b4ca5c26a5f13ca467fbc
jrboyd <jrboyd@med.uvm.edu>
2022-02-08 09:38:16 -0500

    run_by_match passes tests for FRIP

--- R/functions_run_by_match.R
+++ R/functions_run_by_match.R
@@ -1,4 +1,4 @@
-run_by_match = function(object, FUN){
+run_by_match = function(name, object = NULL, callFUN = NULL){
   must_match = object@matched_only
   query_gr = object@features_config$assessment_features[[name]]
   sig_configs = .make_query_signal_config(object@signal_config)
@@ -7,16 +7,22 @@ run_by_match = function(object, FUN){
     if(!sig_name %in% names(sig_configs)){
       must_match = FALSE
     }else{
-      out = lapply(sig_configs[feature_name2signal_name(name)], FUN, query_gr = query_gr, sel_sig_config = sel_sig_config, bfc = bfc)    
+      out = lapply(sig_configs[feature_name2signal_name(name)], 
+                   callFUN, 
+                   query_gr = query_gr, 
+                   bfc = object@bfc)    
     }
   }
   if(!must_match){
-    out = lapply(sig_configs, FUN, query_gr = query_gr, sel_sig_config = sel_sig_config, bfc = bfc)      
+    out = lapply(sig_configs, 
+                 callFUN, 
+                 query_gr = query_gr, 
+                 bfc = object@bfc)      
   }
   out
 }
 
-make_frip_dt.run_by_match = function(query_gr, sel_sig_config, bfc){
+make_frip_dt.run_by_match = function(sel_sig_config, query_gr, bfc){
   make_frip_dt(as.data.table(sel_sig_config@meta_data), 
                query_gr = query_gr, 
                color_var = sel_sig_config@color_by, 
